3d96470c1d739c4af0e581016902690d9f4b19e8
Kevin Givens <givenskevinm@gmail.com>
2022-03-18 21:13:21 -0400

    add cb client data

---
+++ mxts/exchange/coinbase/data.py
@@ -0,0 +1,57 @@
+from typing import Any, AsyncIterator, Awaitable, Callable, List, Optional, Type
+
+from pydantic import BaseModel
+from pandas import Timestamp
+
+class Ticker(BaseModel):
+    trade_id: int
+    price: float
+    size: float
+    time: Timestamp # str frmt '2022-03-16T15:44:59.735466Z'
+    bid: float
+    ask: float
+    volume: float
+
+class Stats(BaseModel):
+    open: float
+    high: float
+    low: float
+    last: float
+    volume: float
+    volume_30day: float
+
+
+class Candle(BaseModel):
+    times: Timestamp
+    low: float
+    high: float
+    open: float
+    close: float
+    volume: float
+
+
+class CurrencyDetail(BaseModel): 
+    type: str
+    symbol: Optional[str]
+    network_confirmations: int
+    sort_order: int
+    crypto_address_link: str
+    crypto_transaction_link: str
+    push_payment_methods: List[str]
+    group_types: Optional[List]
+    display_name: Optional[str]
+    processing_time_seconds: Optional[int]
+    min_withdrawal_amount: float
+    max_withdrawal_amount: float
+
+
+class Currency(BaseModel):
+    id: str
+    name: str
+    min_size: float
+    status: str
+    message: Optional[str]
+    max_precision: float
+    convertible_to: Optional[List]
+    details: CurrencyDetail
+   
