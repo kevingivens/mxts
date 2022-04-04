7178df3405de5881b1683870ea1396835a03b7e2
Kevin Givens <givenskevinm@gmail.com>
2022-04-04 14:28:56 -0400

    rename cb exchange

--- mxts/exchange/coinbase/exchange.py
+++ mxts/exchange/coinbase/exchange.py
@@ -1,22 +1,21 @@
-import os
 from typing import List, AsyncGenerator, Any
 
-from mxts.core import ExchangeType, Order, Instrument, Position, Event
-from mxts.config import TradingType, InstrumentType
+# from mxts.core import ExchangeType, Order, Instrument, Position, Event
+# from mxts.config import TradingType, InstrumentType
 
+from mxts.core import Event
+from mxts.config import TradingType
 
 from yarl import URL
 
-#from mxts.exchange import Exchange
-
+# from mxts.exchange import Exchange
 from .client import CoinbaseClient
 
 
-
-#class CoinbaseProExchange(Exchange):
-
 class CoinbaseProExchange():
     """Coinbase Pro Exchange
+    
+        TODO: inherit from Exchange base class to e
     Args:
         trading_type (TradingType): type of trading to do. Must be Sandbox or Live
         verbose (bool): run in verbose mode
@@ -32,8 +31,7 @@ class CoinbaseProExchange():
         trading_type= TradingType.SANDBOX,
         api_key: str = "",
         api_secret: str = "",
-        api_passphrase: str = "",
-        **kwargs: dict
+        api_passphrase: str = ""
     ) -> None:
         self._trading_type = trading_type
         self._verbose = verbose
@@ -43,30 +41,26 @@ class CoinbaseProExchange():
             TradingType.LIVE: URL("https://api.exchange.coinbase.com")
         }
 
-        # coinbase keys
-        self._api_key = api_key
-        self._api_secret = api_secret
-        self._api_passphrase = api_passphrase
-
-        # Create an exchange client based on the coinbase docs
-        # Note: cbpro doesnt seem to work as well as I remember,
-        # and ccxt has moved to a "freemium" model where coinbase
-        # pro now costs money for full access, so here i will just
-        # implement the api myself.
+        # Create an exchange client
         self._client = CoinbaseClient(
             base_urls[self._trading_type],
             # self.exchange(),
-            self._api_key,
-            self._api_secret,
-            self._api_passphrase,
+            api_key,
+            api_secret,
+            api_passphrase,
         )
 
         # list of market data subscriptions
         # self._subscriptions: List[Instrument] = []
         self._subscriptions = ['BTC-USD']
     
-     async def tick(self) -> AsyncGenerator[Any, Event]:  # type: ignore[override]
-         async with self._client as client:
-             while True:
-                ticker = await self._client.get_ticker(self._subscriptions)
-                yield ticker
+    async def tick(self) -> AsyncGenerator[Any, Event]:  # type: ignore[override]
+        async with self._client as client:
+            while True:
+               ticker = await self._client.get_ticker(self._subscriptions)
+               yield ticker
+    
+    async def instruments(self) -> List[Instruments]:  # type: ignore[override]
+        async with self._client as client:
+            ret = await self._client.get_currencies()
+
